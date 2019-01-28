dbc5df898edaba05057db389f722d91a6e637729
Jonathan Alvarez G <j@jonalvarezz.com>
2019-01-27 22:32:27 -0500

    Adds Spinner

--- src/components/TariffList.js
+++ src/components/TariffList.js
@@ -1,9 +1,15 @@
 import React, { useContext, useEffect } from 'react';
 import styled from 'styled-components';
+import Spin from 'antd/lib/spin';
 import StoreContext from '../store/context';
 import { setData } from '../store/actions';
 import { fetchData } from '../apis/api';
 import Tariff from './Tariff';
+
+const Spinner = styled.div`
+  text-align: center;
+  padding: 60px 0;
+`;
 
 const List = styled.ul`
   padding: 0;
@@ -15,13 +21,21 @@ const List = styled.ul`
 `;
 
 function TariffList() {
-  const { dispatch, list } = useContext(StoreContext);
+  const { dispatch, list, status } = useContext(StoreContext);
 
   useEffect(() => {
     fetchData().then(data => {
       dispatch(setData(data));
     });
   }, []);
+
+  if (status !== 'fetched') {
+    return (
+      <Spinner>
+        <Spin tip="Loading..." />
+      </Spinner>
+    );
+  }
 
   return (
     <List>
