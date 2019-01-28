3c69efdc5a292b8c9aec519317e736f66c22a012
Jonathan Alvarez G <j@jonalvarezz.com>
2019-01-28 00:45:41 -0500

    Updates

---
+++ precache-manifest.d6dd3d63a37424e6439b78b682db18e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bc9ff3a07853ca66d27",
+    "url": "/tarifer-app/static/js/runtime~main.5bc9ff3a.js"
+  },
+  {
+    "revision": "be557ddff0c11b8919fc",
+    "url": "/tarifer-app/static/js/main.be557ddf.chunk.js"
+  },
+  {
+    "revision": "9929125569380ac6b6f5",
+    "url": "/tarifer-app/static/js/1.99291255.chunk.js"
+  },
+  {
+    "revision": "9929125569380ac6b6f5",
+    "url": "/tarifer-app/static/css/1.e53177c4.chunk.css"
+  },
+  {
+    "revision": "db3a352d158e20afd364749ebef37e79",
+    "url": "/tarifer-app/index.html"
+  }
+];
